8c58236581dec8e1796dfbe0238b8a04877344c9
Luis Carlos <luiscbravo94@gmail.com>
2024-09-09 23:43:12 +0100

    bug fixed at chatacter/:name

--- backEnd/routes/routes.js
+++ backEnd/routes/routes.js
@@ -2,14 +2,12 @@
 const express = require("express")
 const axios = require("axios")
 const router = express.Router()
-const API = 'https://rickandmortyapi.com/api/character'
+const API = 'https://rickandmortyapi.com/api/character/?name='
 
 router.get("/characters",async(req,res)=>{
     try {
         const response = await axios.get(API)
         const characters = await response.data.results
-        
-        
         res.json(characters)
     } catch (error) {
         console.error(error.message);
@@ -19,21 +17,17 @@ router.get("/characters",async(req,res)=>{
    
     
 })
-router.get("/characters/:id",async(req,res)=>{
-    const id = req.params.id
+router.get("/character/:name",async(req,res)=>{
+    const name = req.params.name
     
     try {
-        const response = await axios.get(`${API}/${id}`)
+        const response = await axios.get(`${API}${name}&`)
         const characters = await response.data
-        
         res.json(characters)
     } catch (error) {
         console.error(error.message);
-        res.status(404).json({message: `Character not found.`})
-        
+        res.status(404).json({message: `Character not found.`})     
     }
-   
-    
 })
 router.use((req,res) => {
     res.send(" <h1>Page not found</h1> ")
